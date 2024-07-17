945d5de5148dae43cbe696b932d27f743d2f3daa
Mauricio Rohling <mauricio.uto@hotmail.com>
2024-07-17 14:50:58 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 from datetime import datetime
 import os
+import subprocess
 
 app = Flask(__name__)
 
@@ -12,6 +13,14 @@ def hello_world():
 def get_current_time():
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return f'The current time is: {current_time}'
+
+# Vulnerable Route
+@app.route('/list')
+def list_dir():
+    dir_to_list = request.args.get('dir', '')  # Get directory from user input
+    command = f'ls {dir_to_list}'  # Construct command with user input
+    result = subprocess.check_output(command, shell=True)  # Execute command
+    return result.decode()
 
 if __name__ == '__main__':
     app.run()
