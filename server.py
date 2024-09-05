abd1b4400ab5f99d270ab15ef6e01bc294418339
Mauricio Rohling <mauricio.uto@hotmail.com>
2024-09-05 16:59:03 -0300

    Update server.py

--- server.py
+++ server.py
@@ -15,12 +15,12 @@ def get_current_time():
     return f'The current time is: {current_time}'
 
 # Vulnerable Route
-@app.route('/list')
-def list_dir():
-    dir_to_list = request.args.get('dir', '')  # Get directory from user input
-    command = f'ls {dir_to_list}'  # Construct command with user input
-    result = subprocess.check_output(command, shell=True)  # Execute command
-    return result.decode()
+#@app.route('/list')
+#def list_dir():
+#    dir_to_list = request.args.get('dir', '')  # Get directory from user input
+#    command = f'ls {dir_to_list}'  # Construct command with user input
+#    result = subprocess.check_output(command, shell=True)  # Execute command
+#    return result.decode()
 
 if __name__ == '__main__':
     app.run()
